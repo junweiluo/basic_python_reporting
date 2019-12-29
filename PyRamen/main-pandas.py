95a4abe58355446c433f1232b58ef315554a7a41
Junwei Luo <junwei.luo9777@gmail.com>
2019-12-28 20:30:10 -0500

    improved pandas version

--- PyRamen/main-pandas.py
+++ PyRamen/main-pandas.py
@@ -15,8 +15,20 @@ salespath = Path("PyRamen/Resources/sales_data.csv")
 menu = pd.read_csv(menupath)
 sales = pd.read_csv(salespath)
 
-# merge 2 dataframe
-df = pd.merge(sales, menu, left_on = "Menu_Item", right_on="item", how="inner")
+# merge 2 dataframe: left join to sales table
+df = pd.merge(sales, menu, left_on = "Menu_Item", right_on="item", how="left", indicator = True)
+
+# filter for unjoined sales item, get unique value and print out unjoined items
+df_unjoined = df[df["_merge"] != "both"]
+
+df_unjoined_unique = df_unjoined["Menu_Item"].unique()
+
+if df_unjoined_unique.size > 0:
+    for item in df_unjoined_unique:
+        print(f"{item} does not equal any item on menu! NO MATCH!")  
+
+# filter for matched sales items
+df = df[df["_merge"] == "both"]
 
 # convert to numeric
 df[["Quantity", "price", "cost"]] = df[["Quantity", "price", "cost"]].apply(pd.to_numeric)
@@ -34,9 +46,7 @@ df_summary = df.groupby(["item"]).sum()
 df_summary2 = df_summary[["01-count","02-revenue","03-cogs","04-profits"]]
 
 # convert dataframe to dict for required reporting
-report = df_summary2.to_dict("index")
-
-        #    print(f"{row[4]} does not equal any item on menu! NO MATCH!")   
+report = df_summary2.to_dict("index") 
 
 #output result
 output_path = Path("PyRamen/output.txt")
@@ -45,4 +55,3 @@ with open(output_path, 'w') as outputfile:
     for k, v in report.items():
         outputfile.write(f"{str(k)} {str(v)}")
         outputfile.write(f"\n")
-        
